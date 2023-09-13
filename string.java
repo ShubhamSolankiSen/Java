fe703627fd709fe0b2c4855de881831cb618e42f
Shubham Solanki <121003151+ShubhamSolankiSen@users.noreply.github.com>
2023-09-13 08:40:39 +0530

    Add files via upload

---
+++ string.java
@@ -0,0 +1,45 @@
+public class string {
+    public static void main(String[] args) {
+        // write String types of two methods
+        
+        String name = "Harry";
+
+        // String Methods
+
+        // String name = new String("Shubh");
+        // System.out.println(name.length());
+
+        // String lString = name.toLowerCase();
+        // System.out.println(lString);
+
+        // String uString = name.toUpperCase();
+        // System.out.println(uString);
+
+        // String nonTrimmedString = " Shubh ";
+        // System.out.println(nonTrimmedString);
+
+        // String trimmedString = nonTrimmedString.trim();
+        // System.out.println(trimmedString);
+
+        // System.out.println(name.substring(2));
+        // System.out.println(name.substring(2,5));
+
+        // System.out.println(name.replace('h', 'r'));
+        // System.out.println(name.replace("a" , "ier"));
+
+        // System.out.println(name.startsWith("Ha", 0));
+        // System.out.println(name.endsWith("ry"));
+
+        // System.out.println(name.charAt(4));
+
+        // System.out.println(name.indexOf("arry"));
+
+        // String modifiedName = "Harryrry";
+        // System.out.println(modifiedName.indexOf("rry", 4));
+
+        // System.out.println(modifiedName.lastIndexOf("rry", 4));
+
+        // System.out.println(name.equals("Harry"));
+        // System.out.println(name.equalsIgnoreCase("harry"));
+    }
+}
---
+++ stringsQ.java
@@ -0,0 +1,25 @@
+public class stringsQ {
+    public static void main(String[] args) {
+        // 1. Write a java program to convert a string to lowercase.
+
+        // String name = "Shubham";
+        // System.out.println(name.toLowerCase());
+
+        //2. Write a program to replace spaces with underscores.
+
+        //    String text = "to Lower Case";
+        //    text = text.replace(" ","_");
+        //    System.out.println(text);
+        
+        //3.  Write a java program to fill in a letter template which booke like below:
+
+        // letter = "Dear <|name|> , Thanks a lot"
+        // Replace <|name|> with a string (some name)
+
+            // String letter =  "Dear <|name|> , Thanks a lot";
+            // letter = letter.replace("<|name|>", "Shubham" );
+            // System.out.println(letter);
+
+      
+    }
+}
